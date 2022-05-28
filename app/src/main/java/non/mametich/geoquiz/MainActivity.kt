d98a350562c41aae3e77592dbcba9badfb785062
mametich <mametich@rambler.ru>
2022-05-28 15:32:15 +0500

    Первое упражнение из книги Андроид для профессионалов

--- app/src/main/java/non/mametich/geoquiz/MainActivity.kt
+++ app/src/main/java/non/mametich/geoquiz/MainActivity.kt
@@ -11,8 +11,6 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private val quizViewModel: QuizViewModel by viewModels()
 
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
