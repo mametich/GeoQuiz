c7c1315f5617166ef0a6a1a066f4447d87c6f5b8
mametich <mametich@rambler.ru>
2022-05-28 15:22:11 +0500

    Первое упражнение из книги Андроид для профессионалов

--- app/src/main/java/non/mametich/geoquiz/MainActivity.kt
+++ app/src/main/java/non/mametich/geoquiz/MainActivity.kt
@@ -3,26 +3,51 @@ package non.mametich.geoquiz
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.activity.viewModels
 import non.mametich.geoquiz.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private val quizViewModel: QuizViewModel by viewModels()
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.trueButton.setOnClickListener {onRightAnswer()}
-        binding.falseButton.setOnClickListener {onWrongAnswer()}
+        binding.trueButton.setOnClickListener { onRightAnswer() }
+        binding.falseButton.setOnClickListener { onWrongAnswer() }
+        binding.nextButton.setOnClickListener { onNextQuestion() }
+
+        updateQuestion()
+    }
+
+    private fun onNextQuestion() {
+        quizViewModel.moveToNext()
+        updateQuestion()
     }
 
     private fun onWrongAnswer() {
-        Toast.makeText(this, R.string.correct_toast, Toast.LENGTH_SHORT).show()
+        checkAnswer(false)
     }
-
     private fun onRightAnswer() {
-        Toast.makeText(this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show()
+        checkAnswer(true)
+    }
+    private fun updateQuestion(){
+        val questionTextResId = quizViewModel.currentQuestionText
+        binding.questionTextView.setText(questionTextResId)
+    }
+    private fun checkAnswer(userAnswer: Boolean) {
+        val correctAnswer = quizViewModel.currentQuestionAnswer
+        val messageResId = if (userAnswer == correctAnswer) {
+            R.string.correct_toast
+        } else {
+            R.string.incorrect_toast
+        }
+        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT)
+            .show()
     }
 }
